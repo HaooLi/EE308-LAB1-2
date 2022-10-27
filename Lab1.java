6b4174e910a79c45427c8cd052ac3ae3d4960608
HaooLi <2497276880@qq.com>
2022-10-28 01:21:11 +0800

    第七次测试

--- Lab1.java
+++ Lab1.java
@@ -10,7 +10,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 public class Lab1  {       
-
+  
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
